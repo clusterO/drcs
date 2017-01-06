ac32b9c2d63021fe5d0ce6029c72b41d3400d54b
clusterO <fourmou.m@gmail.com>
2017-01-06 19:54:27 +0100

    server added

---
+++ server.go
@@ -0,0 +1,44 @@
+package network
+
+import (
+	"fmt"
+  "net"
+  "bufio"
+  "io"
+)
+
+func main() {
+  listen()
+}
+
+func dial() {
+  conn, err := net.Dial("tcp", "127.0.0.1:8787")
+  if err != nil {
+    fmt.Print("error: ", err)
+  }
+
+  fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+  status, err := bufio.NewReader(conn).ReadString('\n')
+  fmt.Print("status: ", status)
+}
+
+func listen() {
+  ln, err := net.Listen("tcp", ":9999")
+  if err != nil {
+    fmt.Print("error: ", err)
+  }
+
+  for {
+    conn, err := ln.Accept()
+    if err != nil {
+      fmt.Print("error: ", err)
+    }
+    go handleConnection(conn)
+  }
+}
+
+func handleConnection(c net.Conn) {
+  defer c.Close()
+  io.Copy(c, c)
+  fmt.Printf("Connection from %v closed.\n", c.RemoteAddr())
+}
