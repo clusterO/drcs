acca51a1838913e770ad59ad9494a5afe6b586f5
clusterO <fourmou.m@gmail.com>
2017-01-08 17:33:43 +0100

    init & rename rcs folder

--- func.go
+++ func.go
@@ -2,15 +2,37 @@ package dcrs
 
 import (
 	"fmt"
+  "os"
+  "log"
 )
 
-func init() {
+func initialize(directoryPath string) {
 	fmt.Print("local functionalities of rcs")
+
+	err := os.MkdirAll(directoryPath, os.ModePerm)
+	if err != nil {
+    log.Fatal(err)
+	}
 }
 
-func add() {}
+func add(filename string) {}
+
+func renmae(directoryPath string, newName string) {
+  if _, err := os.Stat(newName)
+  err == nil {
+      fmt.Println(newName, " directory exists")
+	  } else if os.IsNotExist(err) {
+      err = os.Rename(directoryPath, newName)
+      if err != nil {
+        log.Fatal(err)
+      }
+	  } else {
+      
+	  }
+}
+
 func clone() {}
-func log() {}
+func logging() {}
 func diff() {}
 func status() {}
 func pull(url string) {}
