314cd7ab7d5cdaa61e44c0d6be8d033e91bbc9e4
cluster0 <fourmou.m@gmail.com>
2017-04-21 19:58:24 +0100

    dcrs add func test

--- tests/dcrs.go
+++ tests/dcrs.go
@@ -2,9 +2,11 @@ package tests
 
 import (
 	"DCRS/dcrs"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -33,5 +35,36 @@ func TestInit(t *testing.T) {
 	_, err = os.Stat(filepath.Join(tempDir, ".obj", "tracker.txt"))
 	if err != nil {
 		t.Errorf("Expected tracker.txt file to be created, but got error: %v", err)
+	}
+}
+
+func TestAdd(t *testing.T) {
+	// Create a temporary directory for testing
+	tempDir, err := ioutil.TempDir("", "test")
+	if err != nil {
+		t.Fatalf("Failed to create temporary directory: %s", err)
+	}
+	defer os.RemoveAll(tempDir)
+
+	// Create a test file
+	testFile := filepath.Join(tempDir, "test.txt")
+	err = ioutil.WriteFile(testFile, []byte("test"), 0644)
+	if err != nil {
+		t.Fatalf("Failed to create test file: %s", err)
+	}
+
+	// Invoke the Add function
+	dcrs.Add("test.txt", tempDir)
+
+	// Assert that the file was added
+	trackerFile := filepath.Join(tempDir, ".obj", "tracker.txt")
+	trackerData, err := ioutil.ReadFile(trackerFile)
+	if err != nil {
+		t.Fatalf("Failed to read tracker file: %s", err)
+	}
+	trackerContent := string(trackerData)
+	expectedLine := fmt.Sprintf("%s uncommitted", filepath.Join(tempDir, ".obj", "test.txt"))
+	if !strings.Contains(trackerContent, expectedLine) {
+		t.Errorf("Expected line not found in tracker file: %s", expectedLine)
 	}
 }
