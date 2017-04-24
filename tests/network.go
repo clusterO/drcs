29c466ba0aec113df8a92dd2b94cf9bcebe63cab
cluster0 <fourmou.m@gmail.com>
2017-04-24 18:49:52 +0100

    network connect func test

--- tests/network.go
+++ tests/network.go
@@ -2,6 +2,11 @@ package tests
 
 import (
 	network "DCRS/net"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -11,5 +16,77 @@ func TestDial(t *testing.T) {
 
 	if result != expected {
 		t.Errorf("Expected server address %s, but got %s", expected, result)
+	}
+}
+
+func TestConnect(t *testing.T) {
+	// Set up a mock server
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Check the request method and path
+		if r.Method == http.MethodGet && r.URL.Path == "/testpkg.zip" {
+			// Create a test package file
+			testPkg := []byte("Test package content")
+
+			// Write the test package file to the response
+			w.Write(testPkg)
+		} else if r.Method == http.MethodPost && r.URL.Path == "/testpkg" {
+			// Check the request headers
+			fileName := r.Header.Get("File-Name")
+			if fileName != "file1.txt" && fileName != "file2.txt" {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+
+			// Read the uploaded file contents
+			fileContents, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			// Do something with the file contents (e.g., save it to disk)
+			_ = ioutil.WriteFile(fileName, fileContents, 0644)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	}))
+	defer server.Close()
+
+	// Set up a temporary directory for testing
+	tempDir, err := ioutil.TempDir("", "connect-test")
+	if err != nil {
+		t.Fatal("Failed to create temporary directory:", err)
+	}
+	defer os.RemoveAll(tempDir)
+
+	// Test pulling a package
+	network.Connect(server.URL, "8181", tempDir, "testpkg", true)
+
+	// Check if the package file was downloaded
+	packagePath := filepath.Join(tempDir, "testpkg")
+	if _, err := os.Stat(packagePath); os.IsNotExist(err) {
+		t.Error("Failed to download the package file:", err)
+	}
+
+	// Test pushing a package
+	file1Path := filepath.Join(tempDir, "file1.txt")
+	file2Path := filepath.Join(tempDir, "file2.txt")
+	if err := ioutil.WriteFile(file1Path, []byte("File 1 contents"), 0644); err != nil {
+		t.Fatal("Failed to create test file:", err)
+	}
+	if err := ioutil.WriteFile(file2Path, []byte("File 2 contents"), 0644); err != nil {
+		t.Fatal("Failed to create test file:", err)
+	}
+
+	network.Connect(server.URL, "8181", tempDir, "testpkg", false)
+
+	// Check if the files were uploaded
+	uploadedFile1Path := filepath.Join(tempDir, "testpkg", "file1.txt")
+	if _, err := os.Stat(uploadedFile1Path); os.IsNotExist(err) {
+		t.Error("Failed to upload file1.txt:", err)
+	}
+	uploadedFile2Path := filepath.Join(tempDir, "testpkg", "file2.txt")
+	if _, err := os.Stat(uploadedFile2Path); os.IsNotExist(err) {
+		t.Error("Failed to upload file2.txt:", err)
 	}
 }
